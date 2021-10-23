577b362447e3403f9afea873a9762e97a1ed6aa0
NadirTellai <tellainadir@gmail.com>
2021-10-23 22:32:58 +0100

    cors

--- index.js
+++ index.js
@@ -14,7 +14,28 @@ app.use(
       extended: false,
   })
 )
-app.use(cors())
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (["https://admin.jsb-club.com", "https://www.jsb-club.com"].indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+  optionSuccessStatus: 200
+};
+
+// CORS middleware
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(cors(corsOptions));
+app.use(allowCrossDomain);
 app.use(auth)
 app.use('/', route)
 
